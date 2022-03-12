3165ca2c32abd7bdb2ad4a075a1c7a69c8a029bf
SiriusLL <sirius.love.light@gmail.com>
2022-03-11 21:45:40 -0700

    deploy contract to local environment

--- scripts/run copy.js
+++ scripts/run copy.js
@@ -3,16 +3,6 @@ const main = async () => {
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
-
-  // Call Mint function
-  let txn = await nftContract.mintEpicNft();
-  // Wait for txn mined
-  await txn.wait();
-
-  //Mint another NFT
-  txn = await nftContract.mintEpicNft();
-  //wait for mined
-  await txn.wait();
 };
 
 const runMain = async () => {
